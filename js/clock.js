ff25807a8eda50f05cc6a8e540708d8998fa6347
tamaslud <tamaslud@yahoo.com>
2022-08-20 11:41:35 +0200

    unformatted clock running

--- js/clock.js
+++ js/clock.js
@@ -1,1 +1,9 @@
 'use strict'
+
+const showActualTime = () => {
+    let timeValue = new Date().toLocaleTimeString('hu');
+    document.querySelector('.timeDigital').innerHTML = timeValue;    
+};
+
+setInterval(showActualTime, 1000);
+
