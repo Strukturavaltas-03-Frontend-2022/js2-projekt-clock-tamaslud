6be73802c1e6bc930bb547ee4ac2be65d5933c9b
tamaslud <tamaslud@yahoo.com>
2022-08-23 21:47:16 +0200

    fixed digit width, font url corrected

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,15 @@
 'use strict'
 
 const showActualTime = () => {
-    let timeValue = new Date().toLocaleTimeString('hu');
-    document.querySelector('.timeDigital').innerHTML = timeValue;    
+    let timeValue = new Date().toLocaleTimeString('hu').split('');
+    timeValue.length == 7 ? timeValue.unshift(' ') : {};
+
+    document.querySelector('span.hours1').innerHTML = timeValue[0];
+    document.querySelector('span.hours2').innerHTML = timeValue[1];
+    document.querySelector('span.minutes1').innerHTML = timeValue[3];
+    document.querySelector('span.minutes2').innerHTML = timeValue[4];
+    document.querySelector('span.seconds1').innerHTML = timeValue[6];
+    document.querySelector('span.seconds2').innerHTML = timeValue[7];
 };
 
 setInterval(showActualTime, 1000);
